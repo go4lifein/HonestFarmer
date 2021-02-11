cafcd94f99262478d82d7e74e3f3a06e58451779
ankitvat <ankitvat@users.noreply.github.com>
2021-02-11 17:41:03 +0530

    updated homepage

--- src/components/header.js
+++ src/components/header.js
@@ -7,13 +7,14 @@ import {AiFillApple} from '@react-icons/all-files/ai/AiFillApple'
 export default function Header()
 
 {
+  const [nav , showNav] = useState(false)
   return(
     <div className = {headerStyles.header}>
       <div className = {headerStyles.nav}>
       <Link to ="/" className= {headerStyles.logo}>
         <Logo />
       </Link>
-      <div className = {headerStyles.navlink}>
+      <div className = {headerStyles.navlink} nav = {nav}>
         <Link to = "/">Our Journey </Link>
         <Link to = "/">Products</Link>
         <Link to = "/">Stories</Link>
@@ -27,7 +28,7 @@ export default function Header()
           <AiFillApple className = {headerStyles.apple} />
         </Link>
       </div>
-      <div className = {headerStyles.menuIcon}>
+      <div className = {headerStyles.menuIcon} onClick ={() => showNav(!nav)}>
     
         <span></span>
         <span></span>
