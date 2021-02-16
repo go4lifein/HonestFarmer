9b3b2bb51f7c6b1f76464a044a6a5451603facb5
Umakant Vashishtha <umakantvashishtha98@gmail.com>
2021-02-16 13:27:10 +0530

    API setup and Report Page

---
+++ src/DummyData/report.js
@@ -0,0 +1,32 @@
+const report = {
+  "id": 1,
+  "productId": 2,
+  "farmerId": 1,
+  "harvestTime": "2021-02-12T03:04:00.000Z",
+  "visits": 3,
+  "createdAt": "2021-02-15T06:20:43.606Z",
+  "updatedAt": "2021-02-15T06:27:48.688Z",
+  "Farmer": {
+      "id": 1,
+      "name": "Arun Thakur",
+      "image": "http://49.50.64.28/StaticFile/Arun.jpeg",
+      "about": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      "region": "Sonipat, Haryana",
+      "subregion": "Manoli",
+      "latitude": "28.907927",
+      "longitude": "77.179413",
+      "createdAt": "2021-02-13T04:05:00.000Z",
+      "updatedAt": "2021-02-13T04:05:00.000Z"
+  },
+  "Product": {
+      "id": 2,
+      "name": "Anaar/Pomegranate",
+      "image": "http://49.50.64.28/AppFiles/ProductImage/7.png",
+      "category": "Fruits",
+      "about": null,
+      "createdAt": "2021-02-14T02:21:00.000Z",
+      "updatedAt": "2021-02-14T02:21:00.000Z"
+  }
+}
+
+export default report;
