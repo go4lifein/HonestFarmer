088e569bfba7a5e75aae75663921389f982e6b61
ankitvat <ankitvat@users.noreply.github.com>
2021-03-03 11:32:29 +0530

    Added Fade-In's

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -7,23 +7,45 @@ export default function Grid()
     return(
         <div className = {gridStyles.grid} id = "grid" >
             
-            <div className = {gridStyles.section1}>
+            <div className = {gridStyles.section1}
+                 
+            >
                
-                <Link to ="/ProgressiveFarming" className = {gridStyles.row1}>
+                <Link to ="/ProgressiveFarming"
+                     data-sal="fade"
+                     data-sal-delay="10"
+                     data-sal-easing="ease"
+                     data-sal-duration = "250"
+                className = {gridStyles.row1}>
                 <h1>Progressive Farming</h1>
                 </Link>
                
-                <Link to = "/Traceable" className = {gridStyles.row2}>
+                <Link to = "/Traceable"
+                 data-sal="fade"
+                 data-sal-delay="40"
+                 data-sal-easing="ease"
+                 data-sal-duration = "300"
+                className = {gridStyles.row2}>
                 <h1>Traceable Food</h1>
                 </Link>
             </div>
             <div className = {gridStyles.section2}>
-                <Link to ="/ZeroWaste" className = {gridStyles.row3}>
+                <Link to ="/ZeroWaste"
+                 data-sal="fade"
+                 data-sal-delay="10"
+                 data-sal-easing="ease"
+                 data-sal-duration = "250"
+                className = {gridStyles.row3}>
                 <h1>Zero Waste</h1>
               
               
                 </Link>
-                <Link to = "/Farmers" className = {gridStyles.row4}>
+                <Link to = "/Farmers" 
+                 data-sal="fade"
+                 data-sal-delay="40"
+                 data-sal-easing="ease"
+                 data-sal-duration = "300"
+                className = {gridStyles.row4}>
                 <h1>Meet Our Farmers</h1>
                 </Link>
             </div>
